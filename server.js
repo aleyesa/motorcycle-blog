cefbbaced81f9ec570a7acd56c9cdf2ac0d973d6
aleyesa <aaron.leyesa@gmail.com>
2023-11-29 13:08:57 -0700

    testing db connection

--- server.js
+++ server.js
@@ -2,28 +2,30 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const cors = require('cors');
 
 const mysql = require('mysql');
 
-const jawsDB = 'mysql://vgyb1e6tc22029gj:pkl6mmas5wvd31m9@lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/geei9sek17g9jznr';
 
-const connection = mysql.createConnection(jawsDB);
-connection.connect();
- 
-connection.query('SELECT * FROM contents', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
+const JAWSDB_URL = 'mysql://vgyb1e6tc22029gj:pkl6mmas5wvd31m9@lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/geei9sek17g9jznr';
+const db = mysql.createPool(JAWSDB_URL);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  db.query("SELECT * FROM images", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
 });
- 
-// connection.end();
 
 app.use(express.static(__dirname + '/dist/'));
+
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
-});
-
-app.get('/cors', (req, res) => {
-  res.send('This has CORS enabled 🎈')
 });
 
 app.listen(port);
--- src/components/pages/present-tab.js
+++ src/components/pages/present-tab.js
@@ -6,15 +6,21 @@ import ContentLayout from "./layout/content-layout";
 export default class PresentTab extends Component {
     constructor() {
         super();
-
-
     }
+
+    getImages = (e) => {
+        axios.get("mysql://vgyb1e6tc22029gj:pkl6mmas5wvd31m9@lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/geei9sek17g9jznr/", {})
+        .then((data) => {
+            console.data(data);
+        });
+    } 
 
     render() {
 
         return (
             <div>
                 <ContentLayout />
+                {this.getImages()}
                 <img className="present_tab_img" alt="XSR700_img_1" src={require("../../../static/assets/images/XSR700_img_1.jpg")} />
             </div>
         )
