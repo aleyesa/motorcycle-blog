92fb26c4b4aab1cd9b147e32bcbef1dad890d350
aleyesa <aaron.leyesa@gmail.com>
2023-11-28 17:11:43 -0700

    removed db server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const port = process.env.PORT || 8080 || 3306;
+const port = process.env.PORT || 8080;
 const app = express();
 
-const db = require('./webpack/db');
+// const db = require('./webpack/db');
 
-db.query("SELECT * FROM images", (err,result)=>{
-  if(err) {
-  console.log(err)
-  } 
-res.send(result)
-});  
+// db.query("SELECT * FROM images", (err,result)=>{
+//   if(err) {
+//   console.log(err)
+//   } 
+// res.send(result)
+// });  
 
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
